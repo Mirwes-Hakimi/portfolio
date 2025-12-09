f20c4fe5c8b2617bd491e9d04f6f605497ddbcf2
Mirwes-Hakimi <mike.business2033@gmail.com>
2025-12-08 23:27:45 -0800

    added target="_blank" rel="noopener noreferrer"

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -62,7 +62,8 @@ const Portfolio = () => {
         <img src={ src } alt=''
         className='rounded-md duration-200 hover:scale-105' />
         <div className='flex items-center justify-center'>
-          <a href={link} className='w-1/2 px-4 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
+          <a href={link} target="_blank" rel="noopener noreferrer"
+ className='w-1/2 px-4 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
           <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'></button>
         </div>
       </div>
