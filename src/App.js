f480b9aa5080bcdbfadd815e049e1524032045ca
Mirwes-Hakimi <mike.business2033@gmail.com>
2026-04-21 00:41:49 -0700

    fix ESLint errors blocking Vercel build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-import { BrowserRouter as Router, Route, Routes, redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   
   return (
